97ae233fcb58b80e056e2772ceee34de2035c767
Sepide Norouzi <sepidenorouzi17@gmail.com>
2026-07-12 14:47:45 +0330

    initialize settings modal

--- src/components/modals/SettingModal.tsx
+++ src/components/modals/SettingModal.tsx
@@ -4,7 +4,11 @@ import type { ModalProps } from "../../types/modal";
 function SettingModal(props: ModalProps) {
   return (
     <BaseModal {...props} title="Settings">
-      <p>Settings go here.</p>
+      <p>
+        Settings still has no functionality. if you are looking for a dark mode
+        you are in the wrong place and that feature will not be stepping into my
+        app . XoXo
+      </p>
     </BaseModal>
   );
 }
