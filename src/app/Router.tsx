b452048623dae86ef5474cfe9a377fe842f59d4b
Sepide Norouzi <sepidenorouzi17@gmail.com>
2026-07-12 13:20:39 +0330

    fix router bugs

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router";
-import Intro from "../pages/Intro";
 import Dashboard from "../pages/Dashboard";
 import MainLayout from "../layout/MainLayout";
 import Flashcard from "../pages/FlashCard";
@@ -10,14 +9,12 @@ function Router() {
     <div>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Dashboard />} />
         </Route>
 
         <Route element={<StudyLayout />}>
           <Route path="/flashcards" element={<Flashcard />} />
         </Route>
-
-        <Route path="/" element={<Dashboard />} />
       </Routes>
     </div>
   );
