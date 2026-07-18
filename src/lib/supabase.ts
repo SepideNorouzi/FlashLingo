e565f2b5cc8b092916b5e87d7bd1fcb52faf6f42
Sepide Norouzi <sepidenorouzi17@gmail.com>
2026-07-18 11:55:40 +0330

    change supabase variables temporally

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const url = import.meta.env.VITE_SUPABASE_URL;
-const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!url || !anonKey) {
-  throw new Error("Supabase environment variables are missing.");
-}
-
-export const supabase = createClient(url, anonKey);
+export const supabase = url && key ? createClient(url, key) : null;
